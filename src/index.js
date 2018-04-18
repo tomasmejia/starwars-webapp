bb09a7c1afef266478f94497df6078485cc08b6a
Cloudburst <tomas.mejia.ed@hotmail.com>
2018-04-18 13:39:20 -0300

    refactoring peoplelist function

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import People from './People';
 import PeopleList from './PeopleList';
 import { chars } from './chars';
 import 'tachyons';
--- src/peoplelist.js
+++ src/peoplelist.js
@@ -5,12 +5,20 @@ import People from './People';
 const PeopleList = ({ chars }) => {
     return (
         <div>
-            <People name={chars[0].name} gender={chars[0].gender} height={chars[0].height} />
-            <People name={chars[1].name} gender={chars[1].gender} height={chars[1].height} />
-            <People name={chars[2].name} gender={chars[2].gender} height={chars[2].height} />
-            <People name={chars[3].name} gender={chars[3].gender} height={chars[3].height} />
+            {
+              chars.map((char, i) => {
+                return (
+                  <People
+                    id={chars[i].id}
+                    name={chars[i].name}
+                    gender={chars[i].gender}
+                    height={chars[i].height}
+                  />
+                );
+              })
+            }
         </div>
-    )
-}
+    );
+}   
 
 export default PeopleList;
